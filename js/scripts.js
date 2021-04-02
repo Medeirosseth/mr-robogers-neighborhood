1b95b53e73be98f9b0c81661b90d4236f0b1eb75
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-04-02 14:31:43 -0700

    WIP trouble shooting first step

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 ///Business Interface
+
 const numbers = ["0","1","2","3","4","5","6","7","8","9"]
-function robotLanguage(number){
+function robotLanguage(num1, num2){
   const numbers = ["0","1","2","3","4","5","6","7","8","9"]
-  numbers.forEach(function(number) {
-    ;
-  })  
+  const zero = 0;
+    if (zero == 0 ){
+      return true;
+    } 
+    return robotLanguage
 }
 
 
