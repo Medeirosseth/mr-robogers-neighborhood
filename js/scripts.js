13bb0b285b082568fff191e7e82f409de56f90bf
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-04-02 10:52:59 -0700

    WIP trouble shooting first test pt3

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,21 @@
 // const zero = 0 
 // const one = 1
-// function beepboop(zero, one){
-//   if (zero === one){
-//     return "true";
-//   } else {
-//     return "false";
-//   }  
+// function beepboop(number){
+//  if (zero == one){
+//    return true;
+//  }
+  
 // }
-// console.log(beepboop(zero, one))
+// console.log(beepboop(number))
 
+ const text = "hello there "
+ function wordCounter(text) {
+  let wordCount = 0;
+  const wordArray = text.split(" ");
+  wordArray.forEach(function(word) {
+    wordCount++;
+  });
+  return wordCount;
+}
 
-// const zeroToTen = [0,1,2,3,4,5,6,7,8,9,]
-//     let digits = number.split(" ");
-//     digits.forEach(function)
-
-
+console.log(wordCounter(text));
