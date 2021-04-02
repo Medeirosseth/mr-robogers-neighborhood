defc76c463b2b67abfbb3cbc7def397574b34eb6
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-04-02 10:01:31 -0700

    WIP troubleshooting first test, brief brain break

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// const zero = 0 
+// const one = 1
+// function beepboop(zero, one){
+//   if (zero === one){
+//     return "true";
+//   } else {
+//     return "false";
+//   }  
+// }
+// console.log(beepboop(zero, one))
+
+
+// const zeroToTen = [0,1,2,3,4,5,6,7,8,9,]
+//     let digits = number.split(" ");
+//     digits.forEach(function)
+
+
