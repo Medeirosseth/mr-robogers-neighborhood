d83e77027d9657db9e663b50fc570f00b6adafb8
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-04-08 16:58:29 -0700

    add dice roll and name input functionality
    Co-authored-by: Tori West <victoria_west@ymail.com>
    Co-authored-by: Cristina Plesa <cristinaplesa8@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,43 @@
-///Business logic
+// ///Business logic
 
-  function beepboop(num) {
-    let robotSlang = [0,1,2,3,4,5,6,7,8,9,];
-    let robotLanguage = []
-    robotSlang.forEach(function(num) {
-      robotSlang.push(i.toString());
-      if (robotSlang[i].includes(1)) {
-        robotLanguage.push("BEEP!")
-      } else {
-        robotLanguage.push(num);
-      } return robotLanguage 
+  function beepboop(slang) {
+    let robotSlang = ("1,2,3,4,5,5,6");
+    let robotLanguage = robotSlang.map(function(slang) {
+      if (slang.includes("3")) {
+        return "Wont you be my neighbor";
+      } else if ( slang.includes("2")) {
+        return "BEEP!";
+      } else (slang.includes("1")); {
+        return "BOOP!";
+      }
+      
     })
-  }  
+  };  
+
+
+console.log(robotSlang(3))
+
+
+
 //User Interface
 $(document).ready(function(){
   $("#formOne").submit(function(event) {
     event.preventDefault();
    let robotSlang = $("input#translator").val()
-    $("#results").text(robotLanguage);
+    $("#results").text(robotSlang);
 
   });
+  robotSlang
 });
 
-// function beepboop(num) {
-//   let robotSlang = [];
-//   for(let i = 0; i <= 50 ; i++){
-//     robotSlang.push(i.toString());
-//     if (robotTalk[i].include(1)) {
-//     robotSlang.push("Beep");
-//    } else {
-//       robotSlang.push(num);
-//     }
-// } return robotSlang
-// }
+// // function beepboop(num) {
+// //   let robotSlang = [];
+// //   for(let i = 0; i <= 50 ; i++){
+// //     robotSlang.push(i.toString());
+// //     if (robotTalk[i].include(1)) {
+// //     robotSlang.push("Beep");
+// //    } else {
+// //       robotSlang.push(num);
+// //     }
+// // } return robotSlang
+// // }
